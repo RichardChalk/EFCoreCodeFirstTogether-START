9b7c3748d5fa7335d3758f11c74d6d808e42f4c1
Richard Chalk <barmybrit@gmail.com>
2024-10-30 20:53:56 +0100

    Updated DbContext with summaries

--- EFCoreCodeFirstTogether-START/Data/ApplicationDBContext.cs
+++ EFCoreCodeFirstTogether-START/Data/ApplicationDBContext.cs
@@ -12,21 +12,39 @@ namespace EFCoreCodeFirstTogether_START.Data
     {
         // 6: Create DBContext(boiler plate code). Create options & connectionstring variables(boiler plate code).
 
+        // DbSet-skikt för att representera tabellerna i databasen.
+        // Varje DbSet skapar en "tabell" i databasen för respektive typ.
         public DbSet<Person> Person { get; set; }
         public DbSet<Invoice> Invoice { get; set; }
         public DbSet<County> County { get; set; }
 
+
+        /// <summary>
+        /// Tom konstruktor: Denna tomma konstruktor behövs om du vill använda migrations
+        /// (dvs. skapa databasen stegvis baserat på ändringar i datamodellen).
+        /// </summary>
         public ApplicationDbContext()
         {
-            // en tom konstruktor behövs för att skapa migrations
         }
 
+        /// <summary>
+        /// Konstruktor med alternativ (options):
+        /// Denna konstruktor tar in inställningar som skickas från appens konfiguration,
+        /// t.ex. anslutningssträngen.
+        /// </summary>
+        /// <param name="options"></param>
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
 
-        // Denna behövs första gången man kör för att skapa databasen!
+
+        /// <summary>
+        /// Metoden `OnConfiguring`: används första gången applikationen körs för att
+        /// koppla databasen till rätt server.
+        /// Om anslutningssträngen inte redan är inställd, anger vi en direkt här.
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
